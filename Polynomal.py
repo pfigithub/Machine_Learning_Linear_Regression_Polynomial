b8dfab4b1e784f6339c1ffba451da9919854e2e5
pfigithub <pampam7072@gmail.com>
2024-02-27 16:17:05 +0330

    ploting

--- Polynomal.py
+++ Polynomal.py
@@ -30,3 +30,19 @@ poly = PolynomialFeatures(degree=2)
 train_x_poly = poly.fit_transform(train_x)
 train_x_poly
 
+# detail for plot if you want
+clf = linear_model.LinearRegression()
+train_y_ = clf.fit(train_x_poly, train_y)
+# The coefficients
+print ('Coefficients: ', clf.coef_)
+print ('Intercept: ',clf.intercept_)
+
+# ploting data
+import matplotlib.pyplot as plt
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf.intercept_[0]+ clf.coef_[0][1]*XX+ clf.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+
